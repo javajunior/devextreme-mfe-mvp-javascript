d68a67495eb00b59189c93e7b187f88d6c76f783
ggiavatto <ggiavatto@visualsoftware.it>
2024-06-14 18:13:55 +0200

    replace Loading Button... with LoadPanel from devextreme

--- host/src/pages/tasks/tasks.js
+++ host/src/pages/tasks/tasks.js
@@ -1,4 +1,5 @@
 import React from "react";
+import LoadPanel from "devextreme-react/load-panel";
 
 const TaskList = React.lazy(() => import("remote1/TaskList"));
 
@@ -6,7 +7,7 @@ export default function Task() {
   return (
     <React.Fragment>
       <h2 className={"content-block"}>Tasks</h2>
-      <React.Suspense fallback="Loading Button...">
+      <React.Suspense fallback={<LoadPanel visible={true} />}>
         <TaskList />
       </React.Suspense>
     </React.Fragment>
